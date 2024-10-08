2ce6e5fd8f4a9bd66b5963d595e2b99a1fec149f
nasaprayas <161242047+nasaprayas@users.noreply.github.com>
2024-10-08 11:27:17 +0530

    Update Library_Tables.sql

--- Library_Tables.sql
+++ Library_Tables.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE Library;
 USE LIBRARY;
 CREATE TABLE BOOK(
-	Book_id VARCHAR(10) PRIMARY KEY,
+    Book_id VARCHAR(10) PRIMARY KEY,
     Author_id VARCHAR(10) NOT NULL,
     Book_Title VARCHAR(80) NOT NULL,
     Publisher_id VARCHAR(10) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE BOOK(
     Category VARCHAR(50) NOT NULL,
     Price float NOT NULL, 
     Language_name VARCHAR(50) NOT NULL,  
-	Subject_name VARCHAR(50) NOT NULL,
+    Subject_name VARCHAR(50) NOT NULL,
     Genre VARCHAR(50) NOT NULL,
     Date_of_publishing date NOT NULL,
     Date_of_addition date NOT NULL,
@@ -19,27 +19,27 @@ CREATE TABLE BOOK(
     Bought_on date NOT NULL,
     foreign key (Author_id) references Author(Author_id),
     foreign key (Publisher_id) references Publisher(Publisher_id),
-	foreign key (Vendor_Id) references Vendoe(Vendor_id),
-	foreign key (Shelf_id) references Shelf(Shelf_id)
+    foreign key (Vendor_Id) references Vendor(Vendor_id),
+    foreign key (Shelf_id) references Shelf(Shelf_id)
 );
 
 Create Table Author(
-	Author_id Varchar(10) Primary Key,
+    Author_id Varchar(10) Primary Key,
     Author_name varchar(50) NOT NULL
 );
 
 CREATE TABLE Publisher(
-	Publisher_id Varchar(10) Primary Key,
+    Publisher_id Varchar(10) Primary Key,
     Publisher_name varchar(50) NOT NULL
 );
 
 CREATE TABLE Vendor(
-	Vendor_id VARCHAR(10),
+    Vendor_id VARCHAR(10),
     Vendor_name VARCHAR(50)
 );
 
 CREATE TABLE Shelf(
-	Shelf_id VARCHAR(10) PRIMARY KEY,
+    Shelf_id VARCHAR(10) PRIMARY KEY,
     Quantity INT PRIMARY KEY,
     Shelf_floor INT NOT NULL,
     Shelf_rows INT NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE Shelf(
 );
 
 CREATE TABLE EMPLOYEE(
-	Employee_id VARCHAR(10) PRIMARY KEY,
+    Employee_id VARCHAR(10) PRIMARY KEY,
     Employee_name VARCHAR(50) NOT NULL,
     Employee_email VARCHAR(50) NOT NULL,
     Employee_phone INT NOT NULL,
@@ -57,7 +57,7 @@ CREATE TABLE EMPLOYEE(
 );
 
 CREATE TABLE Member(
-	Member_id Varchar(10) NOT NULL,
+    Member_id Varchar(10) NOT NULL,
     Member_name VARCHAR(50) NOT NULL,
     Member_Type Varchar(50) NOT NULL,
     Date_of_birth date NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE Member(
 	);
     
 CREATE TABLE Fine(
-	Fine_id VARCHAR(10) primary key,
+    Fine_id VARCHAR(10) primary key,
     Amount float NOT NULL,
     Days_Delay date NOT NULL,
     Member_id VARCHAR(10) NOT NULL,
@@ -78,9 +78,18 @@ CREATE TABLE Fine(
 );
 
 CREATE TABLE Book_Issue(
-	Issue_id VARCHAR(10) Primary Key,
+    Issue_id VARCHAR(10) Primary Key,
     Issue_date datetime NOT NULL,
-    Return_date datetime,
+    Employee_id Varchar(10) NOT NULL,
+    Member_id VARCHAR(10) NOT NULL,
+    Book_id VARCHAR(10) NOT NULL,
+    foreign key (Employee_id) references Employee(employee_id),
+    foreign key (Member_id) references Member(Member_id),
+    foreign key (Book_id) references Book(Book_id)
+);
+CREATE TABLE Book_Return(
+    Return_id VARCHAR(10) Primary Key,
+    Return_date datetime NOT NULL,
     Employee_id Varchar(10) NOT NULL,
     Member_id VARCHAR(10) NOT NULL,
     Book_id VARCHAR(10) NOT NULL,
