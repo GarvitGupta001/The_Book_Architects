728f45a3325227d002849063b0f801e2f7257388
JhanviN <nagorijhanvi@gmail.com>
2024-11-06 21:21:05 +0530

    Update config.py

--- dbms_library_management_system_backend/config/config.py
+++ dbms_library_management_system_backend/config/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config_:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password/9@localhost/library_management_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/library_management_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
